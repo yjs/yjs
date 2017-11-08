0310500c4e5aac054446492afbe9f8108d6cc28d
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2017-11-07 20:34:44 -0800

    add YEvent

---
+++ src/Util/YEvent.js
@@ -0,0 +1,27 @@
+
+export default class YEvent {
+  constructor (target) {
+    this.target = target
+  }
+  get path () {
+    const path = []
+    let type = this.target
+    const y = type._y
+    while (type._parent !== y) {
+      let parent = type._parent
+      if (type._parentSub !== null) {
+        path.push(type._parentSub)
+      } else {
+        // parent is array-ish
+        for (let [i, child] of parent) {
+          if (child === type) {
+            path.push(i)
+            break
+          }
+        }
+      }
+      type = parent
+    }
+    return path
+  }
+}
