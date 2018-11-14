f94653424af1b542a0727cb9432fa2383878356f
Kevin Jahns <kevin.jahns@protonmail.com>
2018-11-14 07:20:06 +0100

    add prosemirror tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,4 @@
 // TODO: include all tests
-
 import './red-black-tree.js'
 import './y-array.tests.js'
 import './y-text.tests.js'
@@ -7,3 +6,4 @@ import './y-map.tests.js'
 import './y-xml.tests.js'
 import './encode-decode.tests.js'
 import './diff.tests.js'
+import './prosemirror.test.js'
