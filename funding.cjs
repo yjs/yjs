ab9373c188325579c8402cff36efc4be0e8733d4
Kevin Jahns <kevin.jahns@pm.me>
2020-12-18 16:17:38 +0100

    funding message

---
+++ funding.cjs
@@ -0,0 +1,11 @@
+
+const log = require('lib0/dist/logging.cjs')
+
+log.print()
+log.print(log.BOLD, log.GREEN, log.BOLD, 'Thank you for using Yjs ', log.RED, '❤\n')
+log.print(log.GREY, 'The project has grown considerably in the past year. Too much for me to maintain in my spare time.')
+log.print(log.GREY, 'Many companies build their products using this technology. Yet, this project receives very little funding.')
+log.print(log.GREY, 'Yjs is far from done. I want to create more awesome extensions and work on the open issues that I')
+log.print(log.GREY, 'currently don\'t have time for. Dear user, the future of this project entirely depends on you.\n')
+log.print(log.BLUE, log.BOLD, 'Please start funding the project now: https://github.com/sponsors/dmonad \n')
+log.print(log.GREY, '(This message will be removed when I achieved my funding goal)\n\n')
