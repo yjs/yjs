a9b92b9099e70f1543d4f5884f1b5de115412145
Kevin Jahns <kevin.jahns@pm.me>
2021-10-15 19:14:11 +0200

    13.5.15

--- src/index.js
+++ src/index.js
@@ -93,15 +93,15 @@ const importIdentifier = '__ $YJS$ __'
 
 if (glo[importIdentifier] === true) {
   /**
-   * Dear reader of this error message.
+   * Dear reader of this warning message. Please take this seriously.
    *
    * If you see this message, please make sure that you only import one version of Yjs. In many cases,
-   * your package manager installs two versions of Yjs that are used by different packages of your project.
+   * your package manager installs two versions of Yjs that are used by different packages within your project.
    * Another reason for this message is that some parts of your project use the commonjs version of Yjs
    * and others use the EcmaScript version of Yjs.
    *
-   * This very often leads to issues that are hard to debug. We often need to do constructor checks,
-   * e.g. `struct instanceof GC`. If you importent different versions of Yjs, it is impossible for us to
+   * This often leads to issues that are hard to debug. We often need to perform constructor checks,
+   * e.g. `struct instanceof GC`. If you imported different versions of Yjs, it is impossible for us to
    * do the constructor checks anymore - which might break the CRDT algorithm.
    */
   console.warn('Yjs was already imported. Importing different versions of Yjs often leads to issues.')
