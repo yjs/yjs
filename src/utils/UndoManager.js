2458563dc9d0bebcd1e06fa3d866825e5dbcb950
Namit Chadha <namit.chadha@gmail.com>
2022-01-02 12:39:46 +0000

    only emit stack add event if added

--- src/utils/UndoManager.js
+++ src/utils/UndoManager.js
@@ -186,6 +186,7 @@ export class UndoManager extends Observable {
         }
       })
       const now = time.getUnixTime()
+      let didAdd = false;
       if (now - this.lastChange < captureTimeout && stack.length > 0 && !undoing && !redoing) {
         // append change to last stack op
         const lastOp = stack[stack.length - 1]
@@ -193,6 +194,7 @@ export class UndoManager extends Observable {
         lastOp.insertions = mergeDeleteSets([lastOp.insertions, insertions])
       } else {
         // create a new stack op
+        didAdd = true;
         stack.push(new StackItem(transaction.deleteSet, insertions))
       }
       if (!undoing && !redoing) {
@@ -204,7 +206,9 @@ export class UndoManager extends Observable {
           keepItem(item, true)
         }
       })
-      this.emit('stack-item-added', [{ stackItem: stack[stack.length - 1], origin: transaction.origin, type: undoing ? 'redo' : 'undo', changedParentTypes: transaction.changedParentTypes }, this])
+      if (didAdd) {
+        this.emit('stack-item-added', [{ stackItem: stack[stack.length - 1], origin: transaction.origin, type: undoing ? 'redo' : 'undo', changedParentTypes: transaction.changedParentTypes }, this])
+      }
     })
   }
 
