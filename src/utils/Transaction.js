15a98edf04e76cf433d7234cace585a801356d50
Oliver Geer <o@ogeer.org>
2026-07-10 20:53:33 +0100

    Delete superfluous 'ID' import

--- src/utils/Transaction.js
+++ src/utils/Transaction.js
@@ -12,7 +12,7 @@ import {
   generateNewClientId,
   createID,
   cleanupYTextAfterTransaction,
-  UpdateEncoderV1, UpdateEncoderV2, GC, StructStore, ID, AbstractType, AbstractStruct, YEvent, Doc // eslint-disable-line
+  UpdateEncoderV1, UpdateEncoderV2, GC, StructStore, AbstractType, AbstractStruct, YEvent, Doc // eslint-disable-line
 } from '../internals.js'
 
 import * as map from 'lib0/map'
